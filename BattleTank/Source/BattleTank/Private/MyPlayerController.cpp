45f56fed47f41d9ce17cae26e4832f4887c290ce
Artreii <hpmazur01@gmail.com>
2019-07-17 15:11:33 +0200

    AI Controller Log

---
+++ BattleTank/Source/BattleTank/Private/MyPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller not controlling a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s is the possessed tank."), *ControlledTank->GetName());
+	}
+}
+
+ATank* AMyPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
